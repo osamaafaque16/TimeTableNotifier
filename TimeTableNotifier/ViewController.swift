6b437219ae016925cffb68754b7bc0f8258b6c92
osamaafaque16 <47041866+osamaafaque16@users.noreply.github.com>
2021-03-21 16:38:43 +0500

    Initial Commit

---
+++ TimeTableNotifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTableNotifier
+//
+//  Created by Muhammad Osama Afaque on 21/03/2021.
+//  Copyright © 2021 Muhammad Osama Afaque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
